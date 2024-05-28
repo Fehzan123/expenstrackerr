f33f07524286285bc634cbdc44333c924e2fcdc8
Fehzan123 <mohdfehzan1998@gmail.com>
2024-05-28 21:17:02 +0530

    unfluke

---
+++ src/pata/pata.js
@@ -0,0 +1,15 @@
+Based on premium and	3.42	255425	208.51	0.64	-
+View
+4	strategy_name	3.22	370845	303.47	0.65	-
+View
+5	strategy_name	3.22	370845	303.47	0.65	-
+View
+6	Based on premium wit	3.01	135980	185.77	0.49	-
+View
+7	strategy_name	2.76	267867.5	218.49	0.6	-
+View
+8	Wait and trade_Save	2.62	178252.5	161.9	0.63	-
+View
+9	iron condor	2.44	176420	137.51	0.65	-
+View
+10	strategy_name
